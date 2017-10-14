c3292f53de53557a39318669e2e62dcdd5da8849
Eran Durage <eran.durage@ignitetech.com>
2017-10-14 21:59:15 +1030

    Average calc added

--- proj/proj/proj.cpp
+++ proj/proj/proj.cpp
@@ -11,8 +11,47 @@
 
 using namespace std;
 
+template<class T, const int W>
+struct AverageCalc : public vector<T> {
+	double sum;
+	AverageCalc() : sum(0) {}
+	AverageCalc & insert(const T & t) {
+		push_back(t);
+		sum += t;
+
+		return *this;
+	}
+	double operator()() {
+		return sum / size();
+	}
+};
+
+template<class T, const int W>
+struct MovingAverageCalc: public AverageCalc<T, W> {
+	MovingAverageCalc & insert(const T & t) {
+		if (size() == W) {
+			sum -= *(begin());
+			erase(begin());
+		}
+		push_back(t);
+		sum += t;
+
+		return *this;
+	}
+};
+
+
+
+
 int main()
 {
+	MovingAverageCalc<int, 3> movcalc;
+	movcalc.insert(1).insert(2).insert(3).insert(4).insert(4);
+	cout << movcalc() << endl;
+	AverageCalc<int, 3> avgcalc;
+	avgcalc.insert(1).insert(2).insert(3).insert(4).insert(4);
+	cout << avgcalc() << endl;
+	system("pause");
     return 0;
 }
 
